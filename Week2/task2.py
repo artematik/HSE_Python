b3b268583e0533ea3b492b0ff26ff66e49342210
artematik <adiurev@edu.hse.ru>
2024-09-30 11:20:43 +0300

    Add files via upload

---
+++ Week2/task2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        if n == 1:
+            return "1"
+
+        previous = self.countAndSay(n - 1)
+        result = []
+        count = 1
+
+        for i in range(1, len(previous)):
+            if previous[i] == previous[i - 1]:
+                count += 1
+            else:
+                result.append(str(count))
+                result.append(previous[i - 1])
+                count = 1
+
+        result.append(str(count))
+        result.append(previous[-1])
+
+        return ''.join(result)
